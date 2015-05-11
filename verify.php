d8c1682685a84e2c7a30d0e4bf65aacb8746667f
absolutebit <absolutebit@gmx.com>
2015-05-11 02:52:21 -0400

    Improved randomization

--- verify.php
+++ verify.php
@@ -1,24 +1,45 @@
 <?php
 
-$client_seed = "";
 $server_seed = "";
+$client_seed = "";
 
 $z = hash('sha256', $server_seed);
-$n = hash_hmac('sha256', $client_seed, $server_seed);
-$n = preg_replace("/[^0-9]/", "", $n);
 
-while($winners<6 || $losers<9) {
-	   $x = substr($n, $index, 1);
-	   if($x>5 && $winners<6) {
-		      $winners++;
-		      $ticket = $ticket."1";
-	   }
-	   if($x<=5 && $losers<9) {
-		      $losers++;
-		      $ticket = $ticket."0";
-	   }
-	   $index++;
+function createScratchTicket($server_seed, $client_seed) {
+    
+    $card = array();
+    $scratch_index = array();
+    for ($i = 0; $i < 15; $i++) {
+        $card[] = 0;
+        $scratch_index[] = $i;
+    }
+    
+    $hash = hash_hmac("sha512", $server_seed, $client_seed);
+   
+    $rng_pool = str_split($hash, 4);
+    
+    for ($i = 0; $i < 6; $i++) {
+        
+        $rand = intval($rng_pool [0], 16);
+        array_splice($rng_pool, 0, 1);
+        
+        $rand = $rand % count($scratch_index);
+        
+        $idx = $scratch_index[$rand];
+        array_splice($scratch_index, $rand, 1);
+        
+        $card[$idx] = 1;
+    }
+    
+    $pattern = "";
+    for ($i = 0; $i < count($card); $i++) {
+        $pattern .= (string) $card[$i];
+    }
+    
+    return $pattern;
 }
+
+$ticket = createScratchTicket($server_seed, $client_seed);
 
 echo "Hashed Server Seed: $z\r\n";
 echo "Answer Key: $ticket\r\n";
